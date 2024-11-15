8b475600726df0e89524b9e2d5b38b7c619bd049
Aleksey Tikhonov <leohar@mail.ru>
2024-11-15 07:44:44 +0300

    Create models

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Room(models.Model):
+    name = models.CharField(unique=True)
+    # Flag for private chats (for two) and private chats (?)
+    is_private = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    users = models.ManyToManyField(to=User, related_name='rooms')
+
+    def __str__(self):
+        return self.name
+    
+
+class Message(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey(to=Room,on_delete=models.CASCADE, related_name='room')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='user')
+
+    def __str__(self):
+        return self.text
